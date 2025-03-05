599b9a4c348f10a18a09debd4a203cbe689bbce1
윤찬 <vsvx13@naver.com>
2025-03-05 20:53:32 +0900

    [feat]: Ball Clicker Game Canvas를 이용해 만들기

--- app/src/main/java/com/example/canvas/MainActivity.kt
+++ app/src/main/java/com/example/canvas/MainActivity.kt
@@ -1,17 +1,31 @@
 package com.example.canvas
 
+import android.icu.text.ListFormatter.Width
 import android.os.Bundle
+import android.os.CountDownTimer
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.gestures.detectTapGestures
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxWithConstraints
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.drawBehind
 import androidx.compose.ui.geometry.Offset
@@ -19,9 +33,17 @@ import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.canvas.ui.theme.CanvasTheme
+import kotlinx.coroutines.delay
+import kotlin.math.pow
+import kotlin.math.roundToInt
+import kotlin.math.sqrt
+import kotlin.random.Random
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -30,7 +52,9 @@ class MainActivity : ComponentActivity() {
         setContent {
             CanvasTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-
+                    MainScreen(
+                        modifier = Modifier.padding(innerPadding)
+                    )
 //                    MyCanvas(modifier = Modifier.padding(innerPadding))
 //                    Box(
 //                        modifier = Modifier
@@ -44,6 +68,149 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
+}
+
+@Composable
+fun MainScreen(modifier: Modifier = Modifier) {
+    var points by remember {
+        mutableStateOf(0)
+    }
+    var isTimerRunning by remember {
+        mutableStateOf(false)
+    }
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .padding(10.dp),
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(10.dp),
+            horizontalArrangement = Arrangement.Absolute.SpaceBetween
+        ) {
+            Text(
+                text = "Points : $points",
+                fontSize = 20.sp,
+                fontWeight = FontWeight.Bold
+            )
+
+            Button(
+                onClick = {
+                    isTimerRunning = true
+                    points = 0
+                }
+            ) {
+                Text(text = if (isTimerRunning) "Reset" else "Start")
+            }
+
+            CountDownTimer(
+                isTimerRunning = isTimerRunning
+            ) {
+                isTimerRunning = false
+            }
+        }
+
+        BallClicker(
+            enabled = isTimerRunning
+        ) {
+            points++
+        }
+    }
+}
+
+@Composable
+fun CountDownTimer(
+    modifier: Modifier = Modifier,
+    time: Int = 30000,
+    isTimerRunning: Boolean = false,
+    onTimerEnd: () -> Unit = {}
+) {
+    var curTime by remember {
+        mutableStateOf(time)
+    }
+
+    LaunchedEffect(
+        key1 = curTime, key2 = isTimerRunning
+    ) {
+        if (!isTimerRunning) {
+            curTime = time
+            return@LaunchedEffect
+        }
+
+        if (curTime > 0) {
+            delay(1000L)
+            curTime -= 1000
+        } else {
+            onTimerEnd()
+        }
+    }
+
+    Text(
+        modifier = modifier,
+        text = (curTime / 1000).toString(),
+        fontSize = 20.sp,
+        fontWeight = FontWeight.Bold
+    )
+}
+
+@Composable
+fun BallClicker(
+    modifier: Modifier = Modifier,
+    radius: Float = 100f,
+    enabled: Boolean = false,
+    ballColor: Color = Color.Red,
+    onBallClick: () -> Unit = {}
+) {
+    BoxWithConstraints(modifier = Modifier.fillMaxSize()) {
+        var ballPosition by remember {
+            mutableStateOf(randomOffset(
+                radius = radius,
+                width = constraints.maxWidth,
+                height = constraints.maxHeight
+            ))
+        }
+
+        Canvas(
+            modifier = modifier
+                .fillMaxSize()
+                .pointerInput(enabled) {
+                    if(!enabled) {
+                        return@pointerInput
+                    }
+
+                    detectTapGestures {
+                        //클릭한 포지션의 지점과 원의 지점간의 거리 구하기
+                        val distance = sqrt(
+                            (it.x - ballPosition.x).pow(2) +
+                                    (it.y - ballPosition.y).pow(2)
+                        )
+
+                        if(distance < radius) {
+                            ballPosition = randomOffset(
+                                radius = radius,
+                                width = constraints.maxWidth,
+                                height = constraints.maxHeight
+                            )
+                            onBallClick()
+                        }
+                    }
+                }
+        ) {
+            drawCircle(
+                color = ballColor,
+                radius = radius,
+                center = ballPosition
+            )
+        }
+    }
+}
+
+private fun randomOffset(radius: Float, width: Int, height: Int): Offset {
+    return Offset(
+        x = Random.nextInt(radius.roundToInt(), width - radius.roundToInt()).toFloat(),
+        y = Random.nextInt(radius.roundToInt(), height - radius.roundToInt()).toFloat()
+    )
 }
 
 @Composable
